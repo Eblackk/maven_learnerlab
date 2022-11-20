1a5c4c387d44506d19d6f8c36218a8cdbea13123
Eblackk <aps14blacerig@gmail.com>
2022-11-19 23:13:42 -0500

    updated TestPerson testConstructor

--- src/main/java/com/github/curriculeon/Person.java
+++ src/main/java/com/github/curriculeon/Person.java
@@ -1,5 +1,31 @@
 package com.github.curriculeon;
 
-public class Person {
+import com.sun.xml.internal.bind.v2.model.core.ID;
 
+public class Person {
+    public Person() {
+    }
+
+    private long id; //should be "final"
+    private String name;
+
+
+    public Person(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/test/java/com/github/curriculeon/TestPerson.java
+++ src/test/java/com/github/curriculeon/TestPerson.java
@@ -1,5 +1,20 @@
 package com.github.curriculeon;
 
-public class TestPerson {
+import org.junit.Assert;
 
+public class TestPerson {
+    //Given
+    public void constructorTest(Long expectedId, String expectedName) {
+        //When
+        Person person = new Person(expectedId, expectedName);
+
+        //Then
+        Long actualId = person.getId();
+        String actualName = person.getName();
+
+        Assert.assertEquals(expectedId,actualId);
+        Assert.assertEquals(expectedName, actualName);
+
+
+    }
 }
